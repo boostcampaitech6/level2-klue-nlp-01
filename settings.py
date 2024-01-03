fe1cc1957e0c23a079269eb3b79cb5ed1c8ae05c
Dongeon Kim <60685175+ceo21ckim@users.noreply.github.com>
2024-01-04 00:57:47 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -8,9 +8,16 @@ TEST_DIR = os.path.join(DATA_DIR, 'test')
 
 PARAM_DIR = os.path.join(BASE_DIR, 'parameters')
 OUT_DIR = os.path.join(BASE_DIR, 'results')
+FIG_DIR = os.path.join(BASE_DIR, 'asset')
 
 if not os.path.exists(PARAM_DIR):
     os.mkdir(PARAM_DIR)
 
+if not os.path.exists(DEV_DIR):
+    os.mkdir(DEV_DIR)
+
 if not os.path.exists(OUT_DIR):
     os.mkdir(OUT_DIR)
+    
+if not os.path.exists(FIG_DIR):
+    os.mkdir(FIG_DIR)
