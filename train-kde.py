c0b15f39447467b854c4112cea8ab1d560f9b996
LeeKunHa <kunha98@gmail.com>
2024-01-04 03:08:22 +0000

    Update config.yaml

--- train-kde.py
+++ train-kde.py
@@ -6,6 +6,12 @@ from settings import *
 from metrics import compute_metrics
 from data_utils import ReDataset 
 
+import yaml
+from box import Box
+conf_url = 'config.yaml'
+with open(conf_url, 'r') as f:
+	config_yaml = yaml.load(f, Loader=yaml.FullLoader)
+config = Box(config_yaml)
 
 def train(args):
     train_set = ReDataset(args, types='train')
@@ -18,7 +24,7 @@ def train(args):
         output_dir = f'{PARAM_DIR}/{args.model_name.split("/")[-1]}', 
         save_total_limit=10, 
         save_steps=500, 
-        num_train_epochs=20, 
+        num_train_epochs=args.max_epoch, 
         learning_rate=args.learning_rate, 
         per_device_train_batch_size=args.batch_size, 
         per_device_eval_batch_size=args.batch_size, 
@@ -40,33 +46,35 @@ def train(args):
     )
     
     trainer.train()
-    torch.save(model.load_state_dict(), os.path.join(f'{args.model_name.split("/")[-1]}.pt'))
-    
+    torch.save(model, os.path.join(f'{args.model_name.split("/")[-1]}-{args.batch_size}-{args.learning_rate}.pt'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--model_name', required=True, type=str 
+        '--model_name', default=config.model_name, type=str 
     )
     parser.add_argument(
-        '--max_length', '-len', default=256, type=int
+        '--max_length', '-len', default=config.max_length, type=int
     )
     parser.add_argument(
-        '--num_labels', '-l', default=30, type=int
+        '--num_labels', '-l', default=config.num_labels, type=int
     )
     parser.add_argument(
-        '--batch_size', '-b', default=32, type=int
+        '--batch_size', '-b', default=config.batch_size, type=int
     )
     parser.add_argument(
-        '--weight_decay', '-wd', default=0.01, type=float
+        '--weight_decay', '-wd', default=config.weight_decay, type=float
     )
     parser.add_argument(
-        '--learning_rate', '-lr', default=5e-5, type=float
+        '--learning_rate', '-lr', default=config.learning_rate, type=float
+    )
+    parser.add_argument(
+         '--max_epoch', '-epoch', default=config.max_epoch, type=int
     )
     
     args = parser.parse_args()
-    args.train_path = os.path.join(TRAIN_DIR, 'train-v.0.0.2.csv')
-    args.dev_path = os.path.join(DEV_DIR, 'dev-v.0.0.2.csv')
+    args.train_path = os.path.join(TRAIN_DIR, config.train_path)
+    args.dev_path = os.path.join(DEV_DIR, config.dev_path)
     args.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     
     train(args)
