0c5f4c3c5145bec58a3cbafd76c7084badc83b42
Dongeon Kim <60685175+ceo21ckim@users.noreply.github.com>
2024-01-05 16:46:43 +0900

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -1,44 +1,25 @@
 import pickle as pickle
-import os
-import pandas as pd
 import torch
-from ast import literal_eval
+from tqdm.auto import tqdm 
 
 from torch.utils.data import Dataset 
-from transformers import AutoTokenizer 
-from utils import label_to_num
-
-import yaml
-from box import Box
-conf_url = 'config.yaml'
-with open(conf_url, 'r') as f:
-	config_yaml = yaml.load(f, Loader=yaml.FullLoader)
-config = Box(config_yaml)
 
 class ReDataset(Dataset):
-    def __init__(self, args, types='train'):
-	self.types = types 
-        if types=='train':
-            self.datasets = self.preprocessing(self.load_data(args.train_path))
-            self.labels = label_to_num(self.load_data(args.train_path)['label'])
-        elif types=='dev':
-            self.datasets = self.preprocessing(self.load_data(args.dev_path))
-            self.labels = label_to_num(self.load_data(args.dev_path)['label'])
-        else:
-            self.datasets = self.preprocessing(self.load_data(args.test_path))
-            self.labels = label_to_num(self.load_data(args.test_path)['label'])
-        
-        self.tokenizer = AutoTokenizer.from_pretrained(args.model_name)
-        self.max_length = config.max_length
+    def __init__(self, args, X, y, types='train'):
+        self.types = types 
+        self.tokenizer = args.tokenizer 
+        self.datasets = X
+        self.labels = y 
 
+        self.max_length = args.max_length
         self.datasets = self.tokenizing(self.datasets)
 
     def __getitem__(self, idx):
-	if self.types in ['train', 'dev']:
-	    item = {key: val[idx].clone().detach() for key, val in self.datasets.items()}
-	    item['label'] = torch.tensor(self.labels[idx])
-	    return item
-	else:
+        if self.types in ['train', 'dev']:
+            item = {key: val[idx].clone().detach() for key, val in self.datasets.items()}
+            item['label'] = torch.tensor(self.labels[idx])
+            return item
+        else:
             item = {key: val[idx].clone().detach() for key, val in self.datasets.items()}
             return item, torch.tensor(self.labels[idx])
 
@@ -47,19 +28,10 @@ class ReDataset(Dataset):
 
     def df2list(self, data):
         return data.values.tolist()
-
-    def preprocessing(self, datasets):
-        target_col = ['sentence', 'subject_entity', 'object_entity', 'label']
-        for column in ['subject_entity', 'object_entity']:
-            datasets.loc[:, column] = datasets.loc[:, column].apply(lambda x: literal_eval(x)['word'])
-        return datasets.loc[:, target_col].copy()
     
-    def load_data(self, path):
-        return pd.read_csv(path)
-
     def tokenizing(self, datasets):
         concat_entity = []
-        for idx, rows in datasets.iterrows():
+        for idx, rows in tqdm(datasets.iterrows(), total=datasets.shape[0], desc=f'{self.types.title()}set Tokenizing...'):
             temp = rows['subject_entity'] + '[SEP]' + rows['object_entity'] + '[SEP]' + rows['sentence']
             concat_entity.append(temp)
             
