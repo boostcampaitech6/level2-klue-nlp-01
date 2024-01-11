2767abf6cdc298c0657339c35b566b647013e632
root <ponben@naver.com>
2024-01-11 09:08:40 +0000

    add abspath

--- utils/tapt.py
+++ utils/tapt.py
@@ -1,7 +1,9 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 import torch
 from transformers import AutoTokenizer, DataCollatorForLanguageModeling, AutoModelForMaskedLM, get_linear_schedule_with_warmup, LineByLineTextDataset, Trainer, TrainingArguments, EarlyStoppingCallback 
 import wandb
-import os
 import argparse
 from settings import * 
 
